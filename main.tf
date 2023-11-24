12b076a0809f3362f7dd3fe68685b163122d0f11
Raymund Dela Cerna <49200654+cebuanos@users.noreply.github.com>
2023-11-24 18:18:56 +0800

    added fixed name prefix 04

--- main.tf
+++ main.tf
@@ -103,17 +103,12 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   }
 
   tags = {
     Environment = "Dev"
+    Project     = "TerraformX"
   }
 }
 
