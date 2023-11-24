90638cbc32009da605b44c466cd7832479f016ac
Raymund Dela Cerna <49200654+cebuanos@users.noreply.github.com>
2023-11-24 22:02:25 +0800

    added fixed 12

--- main.tf
+++ main.tf
@@ -72,21 +72,6 @@ module "alb" {
     }
   }
 
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
-    }
-  ]
-
   tags = {
     Environment = "Dev"
   }
